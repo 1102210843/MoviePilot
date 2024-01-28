452dd0642286d803de64d7ab6178321319ff00c5
sunyu8 <sunyu8@wps.cn>
2024-01-28 16:02:17 +0800

    修改构建命令

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ python_output=$(python version.py)
 # 在Shell脚本中使用变量值
 echo "构建版本为: $python_output"
 
-docker build --platform linux/amd64 --no-cache --rm -t 192.168.1.201:5555/moviepilot:"$python_output" .
+docker build --no-cache --rm -t 192.168.1.201:5555/moviepilot:"$python_output" .
 
 docker push 192.168.1.201:5555/moviepilot:"$python_output"
 
